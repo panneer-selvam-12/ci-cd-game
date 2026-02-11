b7ccf05e77fb41d8f5626fbaa037c80bef8ade0d
Panneer Selvam M <panneerselvamm2135@gmail.com>
2026-02-11 10:22:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "game_server" {
-  ami           = "ami-019715e0d74f695be" 
-  instance_type = "t3.micro"
-  key_name      = "jenkinskkp"
+  ami           = "ami-0ba8d27d35e9915fb" 
+  instance_type = "t3.small"
+  key_name      = "jenkin"
 
-  security_groups = [aws_security_group.game-sg-jenkins.name]
+  security_groups = [aws_security_group.game.name]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -22,8 +22,8 @@ resource "aws_instance" "game_server" {
   }
 }
 
-resource "aws_security_group" "gamenew" {
-  name = "gamenew"
+resource "aws_security_group" "game" {
+  name = "game"
 
   ingress {
     from_port   = 22
